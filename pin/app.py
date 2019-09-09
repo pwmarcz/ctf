96d36400ce903263cb696bac85c912e3c3ce665f
Paweł Marczewski <pwmarcz@gmail.com>
2019-09-09 21:54:32 +0200

    Add "pin" example

---
+++ pin/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import time
+
+app = Flask(__name__)
+
+# ''.join(random.choice('1234567890') for i in range(10))
+PIN = '1328061477'
+
+DELAY = 0.2
+
+
+@app.route('/')
+def pin():
+    pin = request.args.get('pin')
+    if pin:
+        if compare(pin, PIN):
+            return render_template('login.html', pin_correct=True)
+        else:
+            return render_template('login.html', pin_incorrect=True)
+    return render_template('login.html')
+
+
+def compare(pin1, pin2):
+    m = max(len(pin1), len(pin2))
+    for i in range(m):
+        time.sleep(0.1)
+        if i >= len(pin1) or i >= len(pin2):
+            return False
+        if pin1[i] != pin2[i]:
+            return False
+    return True
