bb6c8080cb7a363e889953f3f8579075d22d1a22
Paweł Marczewski <pwmarcz@gmail.com>
2019-09-10 21:10:12 +0200

    Create pin/pin2

--- pin/app.py
+++ pin/app.py
@@ -3,29 +3,15 @@ import time
 
 app = Flask(__name__)
 
-# ''.join(random.choice('1234567890') for i in range(10))
-PIN = '1328061477'
-
-DELAY = 0.2
+PIN = '7532'
 
 
 @app.route('/')
 def pin():
     pin = request.args.get('pin')
     if pin:
-        if compare(pin, PIN):
+        if pin == PIN:
             return render_template('login.html', pin_correct=True)
         else:
             return render_template('login.html', pin_incorrect=True)
     return render_template('login.html')
-
-
-def compare(pin1, pin2):
-    m = max(len(pin1), len(pin2))
-    for i in range(m):
-        time.sleep(0.1)
-        if i >= len(pin1) or i >= len(pin2):
-            return False
-        if pin1[i] != pin2[i]:
-            return False
-    return True
